967e74c4724135e521978f52df750205300398ae
woodsandpop <132575779+woodsandpop@users.noreply.github.com>
2023-05-12 00:03:50 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,67 @@
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Streamlit Dashboard          
+st.set_page_config(page_title ="Woods and Pop Ltd", page_icon =":guardsman:", layout ="centered")
+st.image("logo.jpeg", width = 600)
+st.title('Woods and Pop Ltd')
+st.header('🔎AI Stock Forecaster App')
+st.subheader("Text Data Analysis (NLP) of the selected Stock")
+
+finviz_url = 'https://finviz.com/quote.ashx?t='
+tickers = ['AMZN', 'GOOG', 'FB']
+
+news_tables = {}
+for ticker in tickers:
+    url = finviz_url + ticker
+
+    req = Request(url=url, headers={'user-agent': 'my-app'})
+    response = urlopen(req)
+
+    html = BeautifulSoup(response, features='html.parser')
+    news_table = html.find(id='news-table')
+    news_tables[ticker] = news_table
+
+parsed_data = []
+
+for ticker, news_table in news_tables.items():
+
+    for row in news_table.findAll('tr'):
+
+        title = row.a.text
+        date_data = row.td.text.split(' ')
+
+        if len(date_data) == 1:
+            time = date_data[0]
+        else:
+            date = date_data[0]
+            time = date_data[1]
+
+        parsed_data.append([ticker, date, time, title])
+
+df = pd.DataFrame(parsed_data, columns=['ticker', 'date', 'time', 'title'])
+
+vader = SentimentIntensityAnalyzer()
+
+f = lambda title: vader.polarity_scores(title)['compound']
+df['compound'] = df['title'].apply(f)
+df['date'] = pd.to_datetime(df.date).dt.date
+
+# Plot raw data
+def plot_raw_data():
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+	fig.layout.update(title_text='Time Series data analysis with Rangeslider', xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
+	
+plot_raw_data()
+
+plt.figure(figsize=(10,8))
+mean_df = df.groupby(['ticker', 'date']).mean().unstack()
+mean_df = mean_df.xs('compound', axis="columns")
+mean_df.plot(kind='bar')
+plt.show()
